beb90ac4fec1fb4c26a00e9d33ad2405908832ae
ajaysamraj <34670366+ajaysamraj@users.noreply.github.com>
2017-12-19 12:58:04 +0530

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ int a=5,b=5;
+
+ int c,d,e,g,h;
+
+ c=a+b;
+ d=a-b;
+ e=a*b;
+ g=a/b;
+ h=a%b;
+ printf("addition %d",c);
+ printf("subtraction %d",d);
+ printf("multiplicatioin %d",c); 
+ printf("dividion %d",c);
+ printf("module %d",c);
+  
+
+
+}
